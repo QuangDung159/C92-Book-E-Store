bf1a01064ef2313fee8995be565c49fff3f38a8d
Dung Lu <dung@mylong.io>
2024-08-06 15:27:32 +0700

    Feat: - Init

---
+++ src/assets/icons/account-icon.tsx
@@ -0,0 +1,25 @@
+import { COLORS } from '@themes';
+import * as React from 'react';
+import Svg, { Path } from 'react-native-svg';
+
+function AccountIcon({
+  color = COLORS.primaryBlack,
+  width = 18,
+  height = 18
+}) {
+  return (
+    <Svg
+      width={width}
+      height={height}
+      viewBox="0 0 18 18"
+      fill="none"
+    >
+      <Path
+        d="M0 2v14a2 2 0 002 2h14c1.1 0 2-.9 2-2V2c0-1.1-.9-2-2-2H2a2 2 0 00-2 2zm12 4c0 1.66-1.34 3-3 3S6 7.66 6 6s1.34-3 3-3 3 1.34 3 3zm-9 8c0-2 4-3.1 6-3.1s6 1.1 6 3.1v1H3v-1z"
+        fill={color}
+      />
+    </Svg>
+  );
+}
+
+export { AccountIcon };
--- src/navigation/index.tsx
+++ src/navigation/index.tsx
@@ -1,4 +1,4 @@
-import { HomeIcon } from '@assets';
+import { AccountIcon, CartIcon, CategoriesIcon, HomeIcon } from '@assets';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { createStackNavigator } from '@react-navigation/stack';
 import { AccountScreen, CartScreen, CategoriesScreen, HomeScreen } from '@screens';
@@ -11,34 +11,52 @@ const Stack = createStackNavigator();
 const Tab = createBottomTabNavigator();
 
 function BottomTabNavigator() {
+  const renderTabBarIcon = (icon: (color: string) => React.ReactNode, isFocused = false) => {
+    const color = isFocused ? COLORS.primaryWhite : COLORS.primaryBlack
+    if (isFocused) {
+      return (
+        <View style={styles.iconActive}>
+          {icon(color)}
+        </View>
+      )
+    }
+    return (
+      <>{icon(color)}</>
+    )
+  }
+
   return (
     <Tab.Navigator screenOptions={{
       headerShown: false
     }}>
       <Tab.Screen options={{
         tabBarIcon: (focus) => {
-          if (focus.focused) {
-            return (
-              <View style={styles.iconActive}>
-                <HomeIcon color={COLORS.primaryWhite}></HomeIcon>
-              </View>
-            )
-          }
-          return (
-            <HomeIcon></HomeIcon>
-          )
+          return renderTabBarIcon((color) => (<HomeIcon color={color}></HomeIcon>), focus.focused)
         },
-        tabBarLabelStyle: {
-          fontSize: 14,
-          lineHeight: 20,
-          fontWeight: '200',
-          color: COLORS.primaryBlack
-        },
+        tabBarLabelStyle: styles.tabBarLabelStyle,
         tabBarLabel: 'Home'
       }} name={SCREEN_NAME.HOME} component={HomeScreen} />
-      <Tab.Screen name="categories_screen" component={CategoriesScreen} />
-      <Tab.Screen name="cart_screen" component={CartScreen} />
-      <Tab.Screen name="account_screen" component={AccountScreen} />
+      <Tab.Screen options={{
+        tabBarIcon: (focus) => {
+          return renderTabBarIcon((color) => (<CategoriesIcon color={color}></CategoriesIcon>), focus.focused)
+        },
+        tabBarLabelStyle: styles.tabBarLabelStyle,
+        tabBarLabel: 'Categories'
+      }} name={SCREEN_NAME.CATEGORIES} component={CategoriesScreen} />
+      <Tab.Screen options={{
+        tabBarIcon: (focus) => {
+          return renderTabBarIcon((color) => (<CartIcon color={color}></CartIcon>), focus.focused)
+        },
+        tabBarLabelStyle: styles.tabBarLabelStyle,
+        tabBarLabel: 'Cart'
+      }} name={SCREEN_NAME.CART} component={CartScreen} />
+      <Tab.Screen options={{
+        tabBarIcon: (focus) => {
+          return renderTabBarIcon((color) => (<AccountIcon color={color}></AccountIcon>), focus.focused)
+        },
+        tabBarLabelStyle: styles.tabBarLabelStyle,
+        tabBarLabel: 'Account'
+      }} name={SCREEN_NAME.ACCOUNT} component={AccountScreen} />
     </Tab.Navigator>
   );
 }
@@ -61,5 +79,11 @@ const styles = StyleSheet.create({
     borderRadius: 24,
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  tabBarLabelStyle: {
+    fontSize: 14,
+    lineHeight: 20,
+    fontWeight: '200',
+    color: COLORS.primaryBlack
   }
 })
