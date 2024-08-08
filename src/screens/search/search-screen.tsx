a43058da98f142c8e73a5f411a64a1df5d385ef0
Dung Lu <dung@mylong.io>
2024-08-08 15:30:31 +0700

    Feat: - Search screen

--- src/screens/search/search-screen.tsx
+++ src/screens/search/search-screen.tsx
@@ -1,6 +1,6 @@
-import { MaterialCommunityIcons } from '@expo/vector-icons';
-import React from 'react';
-import { ScrollView, StyleSheet, View } from 'react-native';
+import { Entypo, MaterialCommunityIcons } from '@expo/vector-icons';
+import React, { useRef } from 'react';
+import { ScrollView, StyleSheet, TouchableOpacity, View } from 'react-native';
 import { Chip, Layouts, ListBookCardVerticalRow, SearchBar } from '@components';
 import { TOP_BOOKS } from '@constants';
 import { searchStore } from '@store';
@@ -8,6 +8,8 @@ import { COLORS } from '@themes';
 import { SortSection } from './components';
 
 const SearchScreen = ({ route, navigation }: any) => {
+  const scrollRef = useRef<ScrollView>();
+
   const renderFilter = () => {
     return (
       <View style={styles.filterContainer}>
@@ -27,16 +29,31 @@ const SearchScreen = ({ route, navigation }: any) => {
     );
   };
 
+  const scrollToTop = () => {
+    if (scrollRef.current) {
+      scrollRef.current.scrollTo({ x: 0, y: 0, animated: true });
+    }
+  };
+
   return (
     <View style={styles.container}>
       <SearchBar showCartIcon showBackIcon navigation={navigation} />
-      <SortSection onPress={() => {}} label={searchStore.sortOption.label} />
-      <Layouts.VSpace value={12} />
-      {renderFilter()}
-      <Layouts.VSpace value={12} />
-      <ScrollView scrollEnabled={true} showsVerticalScrollIndicator={false}>
+      <ScrollView
+        ref={scrollRef}
+        scrollEnabled={true}
+        showsVerticalScrollIndicator={false}
+      >
+        <SortSection onPress={() => {}} label={searchStore.sortOption.label} />
+        <Layouts.VSpace value={12} />
+        {renderFilter()}
+        <Layouts.VSpace value={12} />
         <ListBookCardVerticalRow listItem={TOP_BOOKS} />
       </ScrollView>
+      <View style={styles.scrollTop}>
+        <TouchableOpacity onPress={scrollToTop}>
+          <Entypo name="chevron-up" size={24} />
+        </TouchableOpacity>
+      </View>
     </View>
   );
 };
@@ -52,6 +69,18 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
     alignItems: 'center',
     flexWrap: 'wrap',
+  },
+  scrollTop: {
+    width: 50,
+    height: 50,
+    borderRadius: 50,
+    backgroundColor: COLORS.gray,
+    alignItems: 'center',
+    justifyContent: 'center',
+    position: 'absolute',
+    bottom: 8,
+    right: 24,
+    opacity: 0.8,
   },
 });
 
