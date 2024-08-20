b8e71c0660a9571ed2ea3bce70efbe0946295cc1
Dung Lu <dung@mylong.io>
2024-08-20 22:04:58 +0700

    Feat: Deeplink

---
+++ src/App.tsx
@@ -0,0 +1,53 @@
+import {
+  NavigationContainer,
+  NavigationContainerRef,
+} from '@react-navigation/native';
+import * as Linking from 'expo-linking';
+import { useEffect, useRef } from 'react';
+import React, { View } from 'react-native';
+
+import Toast from 'react-native-toast-message';
+import { Layouts } from '@components';
+import { useNavigate } from '@hooks';
+import { appModel } from '@store';
+import { Navigation } from 'navigation';
+import { BaseParamList } from './routes';
+
+const App = () => {
+  const url = Linking.useURL();
+  const navigationRef = useRef<NavigationContainerRef<BaseParamList>>(null);
+
+  const { openPaymentSuccessScreen } = useNavigate(navigationRef.current);
+
+  useEffect(() => {
+    appModel.appInit();
+    appModel.loadMasterData();
+  }, []);
+
+  console.log('url :>> ', url);
+
+  if (url) {
+    const { path, queryParams } = Linking.parse(url);
+    console.log('queryParams :>> ', queryParams);
+
+    if (path === 'payment-success') {
+      openPaymentSuccessScreen();
+    }
+  }
+
+  return (
+    <NavigationContainer ref={navigationRef}>
+      <View
+        style={{
+          zIndex: 199,
+        }}
+      >
+        <Toast visibilityTime={2000} topOffset={0} />
+      </View>
+      <Layouts.VSpace value={24} />
+      <Navigation />
+    </NavigationContainer>
+  );
+};
+
+export { App };
