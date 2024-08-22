bb4b98fe60b61b96efd9381267f54df180abd056
Dung Lu <dung@mylong.io>
2024-08-22 11:09:14 +0700

    Feat: Submit google playstore

--- src/screens/account/account-screen.tsx
+++ src/screens/account/account-screen.tsx
@@ -1,12 +1,22 @@
+import Constants from 'expo-constants';
 import { observer } from 'mobx-react-lite';
 import React from 'react';
-import { StyleSheet, View } from 'react-native';
+import { Platform, StyleSheet, Text, View } from 'react-native';
 import { ScreenHeader } from '@components';
 import { userStore } from '@store';
-import { COLORS } from '@themes';
+import { COLORS, FONT_STYLES } from '@themes';
 import { AccountView, AuthenView } from './components';
 
 const AccountScreen = ({ navigation }: any) => {
+  const version = Constants.expoConfig.version;
+  const buildNumber = Constants.expoConfig.ios.buildNumber; // Dành cho iOS
+  const versionCode = Constants.expoConfig.android.versionCode;
+
+  const buildNumberDisplay = Platform.select({
+    ios: buildNumber,
+    android: versionCode.toString(),
+  });
+
   return (
     <View style={styles.container}>
       <ScreenHeader
@@ -15,6 +25,15 @@ const AccountScreen = ({ navigation }: any) => {
         showBackIcon={false}
       />
       {userStore.userProfile ? <AccountView /> : <AuthenView />}
+      <Text
+        style={{
+          ...FONT_STYLES.REGULAR_12,
+          paddingVertical: 4,
+          textAlign: 'center',
+        }}
+      >
+        Version {version} ({buildNumberDisplay})
+      </Text>
     </View>
   );
 };
