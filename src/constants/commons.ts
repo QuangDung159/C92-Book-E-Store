ea64f2cbdabdafd2181783e148c2180dfbd681ec
Dung Lu <dung@mylong.io>
2024-08-22 16:56:56 +0700

    Feat: EAS update

--- src/constants/commons.ts
+++ src/constants/commons.ts
@@ -45,7 +45,7 @@ const enum ERROR_CODES {
 
 const MOMO_REQUEST_TYPE = 'captureWallet';
 
-const CODE_PUSH_NUMBER = '1724317506';
+const CODE_PUSH_NUMBER = '1724320315';
 
 export {
   ADMINISTRATIVE,
