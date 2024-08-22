e5f183655619df484419ef271cb5e055d815d4e2
Dung Lu <dung@mylong.io>
2024-08-22 14:21:30 +0700

    Feat: Submit google playstore

---
+++ scripts/set-env-stg.js
@@ -0,0 +1,25 @@
+/* eslint-disable @typescript-eslint/no-require-imports */
+const fs = require('fs');
+
+// Đường dẫn tới file nguồn và file đích
+const sourceFile = '.env.stg';
+const destinationFile = '.env';
+
+// Đọc nội dung từ file nguồn
+fs.readFile(sourceFile, 'utf8', (err, data) => {
+  if (err) {
+    console.error(`Không thể đọc file ${sourceFile}:`, err);
+    return;
+  }
+
+  // Ghi nội dung vào file đích
+  fs.writeFile(destinationFile, data, (err) => {
+    if (err) {
+      console.error(`Không thể ghi file ${destinationFile}:`, err);
+      return;
+    }
+    console.log(
+      `Đã sao chép nội dung từ ${sourceFile} sang ${destinationFile}`,
+    );
+  });
+});
