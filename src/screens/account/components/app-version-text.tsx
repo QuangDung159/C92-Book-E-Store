d683725b438a43dfd1a73a139f39a0930e24e99b
Dung Lu <dung@mylong.io>
2024-08-23 08:34:47 +0700

    Feat: EAS update

--- src/screens/account/components/app-version-text.tsx
+++ src/screens/account/components/app-version-text.tsx
@@ -1,24 +1,16 @@
 /* eslint-disable import/no-named-as-default */
 import Constants from 'expo-constants';
 import React from 'react';
-import { Platform, StyleSheet, Text } from 'react-native';
+import { StyleSheet, Text } from 'react-native';
 import { CODE_PUSH_NUMBER } from '@constants';
 import { FONT_STYLES } from '@themes';
 
 const AppVersionText: React.FC<any> = () => {
   const version = Constants.expoConfig.version;
-  const buildNumber = Constants.expoConfig.ios.buildNumber; // iOS
-  const versionCode = Constants.expoConfig.android.versionCode; // android
-
-  const buildNumberDisplay = Platform.select({
-    ios: buildNumber,
-    android: versionCode.toString(),
-  });
 
   return (
     <Text style={styles.textStyle}>
-      {process.env.EXPO_PUBLIC_ENV} - {version} ({buildNumberDisplay}) -{' '}
-      {CODE_PUSH_NUMBER}
+      {process.env.EXPO_PUBLIC_ENV} - {version} ({CODE_PUSH_NUMBER})
     </Text>
   );
 };
