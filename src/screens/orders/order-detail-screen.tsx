2ec53e2803fdc572d9c1761608f4fe1fe2e95579
Dung Lu <dung@mylong.io>
2024-08-30 15:19:28 +0700

    Feat: order screen

--- src/screens/orders/order-detail-screen.tsx
+++ src/screens/orders/order-detail-screen.tsx
@@ -1,81 +1,34 @@
 import { observer } from 'mobx-react-lite';
 import React from 'react';
-import { Dimensions, StyleSheet, Text, View } from 'react-native';
-import { SwipeListView } from 'react-native-swipe-list-view';
-import { EmptyListComponent, Icons, ScreenHeader } from '@components';
-import { notificationStore } from '@store';
-import { COLORS, FONT_STYLES } from '@themes';
+import { ScrollView, StyleSheet, View } from 'react-native';
+import { Divider } from 'react-native-paper';
+import { Layouts, ScreenHeader } from '@components';
+import { LIST_ORDER, TOP_BOOKS } from '@constants';
+import { COLORS } from '@themes';
+import { HorizontalListCard } from 'screens/home/components';
+import { ListOrder } from './components';
 
 const OrderDetailScreen = ({ navigation }: any) => {
-  const { width } = Dimensions.get('window');
-  const hiddenItemHeight = 75;
-  const hiddenItemWidth = 75;
-
   return (
     <View style={styles.container}>
       <ScreenHeader title="Order Detail" navigation={navigation} />
-      <SwipeListView
-        keyExtractor={(item) => item.id}
-        showsVerticalScrollIndicator={false}
-        data={notificationStore.listNotification}
-        renderItem={({ item }) => (
-          <View
-            style={[
-              styles.itemContainer,
-              {
-                height: hiddenItemHeight,
-              },
-            ]}
-          >
-            <View style={styles.contentWrapper}>
-              <Text style={styles.title}>{item.title}</Text>
-              <Text style={styles.content} numberOfLines={2}>
-                {item.content}
-              </Text>
-            </View>
-            <View style={styles.dot}>
-              <Icons.DotSingleIcon
-                color={item.readed ? COLORS.primaryWhite : COLORS.primaryBlack}
-                size={30}
-              />
-            </View>
-          </View>
-        )}
-        disableRightSwipe
-        renderHiddenItem={({ item }) => {
-          return (
-            <View
-              style={[
-                styles.hiddenWrapper,
-                {
-                  height: hiddenItemHeight,
-                  width: width - 5,
-                },
-              ]}
-            >
-              <View
-                style={{
-                  width: width - 5 - hiddenItemWidth,
-                }}
-              />
-              <View style={styles.trashIcon}>
-                <Icons.TrashIcon
-                  color={COLORS.primaryWhite}
-                  size={24}
-                  onPress={() => {
-                    console.log('item :>> ', item);
-                  }}
-                />
-              </View>
-            </View>
-          );
-        }}
-        leftOpenValue={hiddenItemWidth}
-        rightOpenValue={-hiddenItemWidth}
-        ListEmptyComponent={() => {
-          return <EmptyListComponent />;
-        }}
-      />
+      <ScrollView showsVerticalScrollIndicator={false}>
+        <ListOrder listOrder={LIST_ORDER} showFullList />
+        <Divider />
+        <View style={styles.wrapper}>
+          <Layouts.VSpace value={12} />
+          <HorizontalListCard
+            listItem={TOP_BOOKS}
+            title="Maybe you will like"
+          />
+          <Layouts.VSpace value={24} />
+          <HorizontalListCard
+            listItem={TOP_BOOKS}
+            title="Viewed"
+            showSeeMore={true}
+          />
+        </View>
+      </ScrollView>
     </View>
   );
 };
@@ -85,40 +38,9 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: COLORS.primaryWhite,
   },
-  itemContainer: {
-    backgroundColor: COLORS.primaryWhite,
+  wrapper: {
     paddingHorizontal: 24,
-    borderBottomColor: COLORS.gray200,
-    borderBottomWidth: 1,
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'space-between',
-    flex: 1,
-  },
-  contentWrapper: {
-    flex: 9,
-  },
-  title: {
-    ...FONT_STYLES.SEMIBOLD_14,
-    marginBottom: 4,
-  },
-  content: {
-    ...FONT_STYLES.REGULAR_14,
-  },
-  dot: {
-    flex: 1,
-    alignItems: 'flex-end',
-  },
-  hiddenWrapper: {
-    backgroundColor: COLORS.error50,
-    alignSelf: 'flex-end',
-    justifyContent: 'flex-end',
-    flexDirection: 'row',
-  },
-  trashIcon: {
-    width: 75,
-    alignItems: 'center',
-    justifyContent: 'center',
+    paddingBottom: 24,
   },
 });
 
