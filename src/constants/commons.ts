7ed83c857e82caef4471aaa2232bc5ad078aeddf
Dung Lu <dung@mylong.io>
2024-08-31 22:53:36 +0700

    Update build version

--- src/constants/commons.ts
+++ src/constants/commons.ts
@@ -45,7 +45,7 @@ const enum ERROR_CODES {
 
 const MOMO_REQUEST_TYPE = 'captureWallet';
 
-const CODE_PUSH_NUMBER = '1725116501';
+const CODE_PUSH_NUMBER = '1725119569';
 
 const PAYMENT_STATUS = {
   success: 'success',
