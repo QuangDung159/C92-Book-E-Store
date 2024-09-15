c64218e1830b625e7ac1333a16cf4ea3c471f354
DungLu <quangdunglu159@gmail.com>
2024-09-15 22:49:57 +0700

    Update build version

--- src/constants/commons.ts
+++ src/constants/commons.ts
@@ -67,7 +67,7 @@ const enum ERROR_CODES {
 
 const MOMO_REQUEST_TYPE = 'captureWallet';
 
-const CODE_PUSH_NUMBER = '1726326718';
+const CODE_PUSH_NUMBER = '1726415370';
 
 const PAYMENT_STATUS = {
   success: 'success',
