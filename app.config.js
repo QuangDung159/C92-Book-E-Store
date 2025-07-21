faae79c5750a2eb8df0dea1f5b0a25925fd0ec9f
DungLu <quangdunglu159@gmail.com>
2025-07-21 17:40:11 +0700

    Update build version

--- app.config.js
+++ app.config.js
@@ -2,8 +2,8 @@ export default {
   expo: {
     name: 'Book E-Store',
     slug: 'c92-book-e-store-v1',
-    version: '1.0.20',
-    runtimeVersion: '1.0.20',
+    version: '1.0.21',
+    runtimeVersion: '1.0.21',
     orientation: 'portrait',
     icon: './src/assets/images/e-book-logo.png',
     scheme: 'c92bookestorev1',
@@ -16,7 +16,7 @@ export default {
     ios: {
       supportsTablet: true,
       bundleIdentifier: 'com.dragonc92team.BookEStoreV1',
-      buildNumber: '10',
+      buildNumber: '11',
       googleServicesFile: './ios/BookEStore/GoogleService-Info.plist',
     },
     android: {
@@ -25,7 +25,7 @@ export default {
         backgroundColor: '#ffffff',
       },
       package: 'com.dragonc92team.BookEStoreV1',
-      versionCode: 68,
+      versionCode: 69,
       googleServicesFile: './android/app/google-services.json',
       permissions: ['ACCESS_FINE_LOCATION', 'ACCESS_COARSE_LOCATION'],
       targetSdkVersion: 35,
