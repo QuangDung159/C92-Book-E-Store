bf5620b6cf9500c420c7e84788ef31acb61be162
DungLu <quangdunglu159@gmail.com>
2025-10-07 16:24:24 +0700

    Update build version

--- android/app/src/main/java/com/dragonc92team/BookEStoreV1/MainActivity.kt
+++ android/app/src/main/java/com/dragonc92team/BookEStoreV1/MainActivity.kt
@@ -1,4 +1,5 @@
 package com.dragonc92team.BookEStoreV1
+import expo.modules.splashscreen.SplashScreenManager
 
 import android.os.Build
 import android.os.Bundle
@@ -9,24 +10,18 @@ import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.fabricEnable
 import com.facebook.react.defaults.DefaultReactActivityDelegate
 
 import expo.modules.ReactActivityDelegateWrapper
-import vn.zalopay.sdk.ZaloPaySDK
-import vn.zalopay.sdk.Environment;
-import android.content.Intent;
 
 class MainActivity : ReactActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
     // Set the theme to AppTheme BEFORE onCreate to support
     // coloring the background, status bar, and navigation bar.
     // This is required for expo-splash-screen.
-    setTheme(R.style.AppTheme);
+    // setTheme(R.style.AppTheme);
+    // @generated begin expo-splashscreen - expo prebuild (DO NOT MODIFY) sync-f3ff59a738c56c9a6119210cb55f0b613eb8b6af
+    SplashScreenManager.registerOnActivity(this)
+    // @generated end expo-splashscreen
     super.onCreate(null)
-      ZaloPaySDK.init(2554, Environment.SANDBOX)
   }
-
-    override fun onNewIntent(intent: Intent) {
-        super.onNewIntent(intent)
-        ZaloPaySDK.getInstance().onResult(intent)
-    }
 
   /**
    * Returns the name of the main component registered from JavaScript. This is used to schedule
